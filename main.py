085d370bb392bc17ffbea7faded4394a7dc63c53
Tyler B <TrekkerStudios@users.noreply.github.com>
2024-06-06 17:39:15 -0400

    cleaned up

--- main.py
+++ main.py
@@ -1,40 +1,22 @@
 import mido
 
 def send_response_cc(outport, cc_number, cc_value, channel=0):
-    """
-    Send a Control Change message.
-    
-    Parameters:
-    - outport: The MIDI output port
-    - cc_number: Control Change number to send
-    - cc_value: Value of the Control Change message
-    - channel: MIDI channel (default is 0)
-    """
     msg = mido.Message('control_change', channel=channel, control=cc_number, value=cc_value)
     outport.send(msg)
     print(f"Sent CC message: {msg}")
 
 def send_program_change(outport, program_number, channel=0):
-    """
-    Send a Program Change message.
-    
-    Parameters:
-    - outport: The MIDI output port
-    - program_number: The program number to change to (0-127)
-    - channel: MIDI channel (default is 0)
-    """
     msg = mido.Message('program_change', channel=channel, program=program_number)
     outport.send(msg)
     print(f"Sent Program Change message: {msg}")
 
 def main():
     input_name = 'mio'
-    output_name = 'Quad Cortex'
+    output_name = 'mio'
     target_cc_number = 0  # Change this to the incoming CC number you want to detect
     response_cc_number = 0  # Change this to the CC number you want to send in response
     response_cc_value = 0  # Change this to the value of the CC message you want to send
-    counter_before_reset = 0
-    program_value = 0
+    program_value = 0 # tracks preset
 
     # Open the input port
     with mido.open_input(input_name) as inport:
@@ -49,12 +31,12 @@ def main():
                     print(f"Received target CC message: {msg}")
 
                     #check if preset 0-128 or 129-256
-                    counter_before_reset = (counter_before_reset + 1)
-                    if counter_before_reset > 127:
+                    if program_value > 127:
                         if response_cc_value == 1:
                             response_cc_value = 0
                         elif response_cc_value == 0:
                             response_cc_value = 1
+                        program_value = 0
 
                     send_response_cc(outport, response_cc_number, response_cc_value)
                     send_program_change(outport, program_value)
